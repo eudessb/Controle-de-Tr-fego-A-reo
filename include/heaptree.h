20e51361caf77a11cae2105b657c28f11d7e517a
eudessb <eudessilvabezerra985@gmail.com>
2024-11-29 20:23:10 -0300

    include

--- include/heaptree.h
+++ include/heaptree.h
@@ -1,3 +1,6 @@
+/**
+ * Declaração de todas as funções
+ * */
 #ifndef FUNCTIONS_C
 #define FUNCTIONS_C
 
@@ -8,21 +11,38 @@ typedef struct Aeronave
     int horario;
     int tipo;
     int emergencia;
-    int prioridade ;
+    int prioridade;
 } Aeronave;
 
-Aeronave calcular_prioridade (Aeronave aeronave);
+typedef struct HEAP
+{
+    Aeronave *aeronaves;
+    int tamanho_maximo;
+    int tamanho_atual;
+} HEAP;
 
-void inserir_aeronave();
+void construir_maxheap(HEAP *heap);
 
-void carregar_aeronaves();
+void heapify(HEAP *heap, int i);
 
-void consultar_mprioridade();
+void calcular_prioridade(Aeronave *aeronave);
 
-void remover_mprioridade();
+void trocar_elementos(Aeronave *a, Aeronave *b);
 
-void atualizar_prioridade();
+HEAP *inicializar_heap(int tamanho_array);
 
-void exibir();
+void inserir_aeronave(HEAP *heap, Aeronave *nova_aeronave);
+
+void carregar_aeronaves(const char *csvfile, HEAP *heap);
+
+void consultar_maior_prioridade(HEAP *heap);
+
+void remover_mprioridade(HEAP *heap);
+
+void atualizar_prioridade(HEAP *heap);
+
+void esvaziar(HEAP *heap);
+
+void exibir(HEAP *heap);
 
 #endif
